77ac2d962c4f02d738dc8609b0303be06b456a09
Fucklur <boryscuber13@gmail.com>
2026-08-12 18:45:27 +0200

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,18 +1,21 @@
-let tris = 0;
 let pLoad = 0;
-
 const gameBootstrapper = document.getElementById("playbtn");
 
 if (gameBootstrapper) {
   gameBootstrapper.addEventListener("click", () => {
-      const newSpan = document.createElement("span");
-      while (pLoad < 100) {
+    const newSpan = document.createElement("span");
+    newSpan.classList.add("loadGame");
+    newSpan.innerText = "Loading game 0%";
 
-      pLoad++
-      newSpan.classList.add("loadGame");
+    gameBootstrapper.replaceWith(newSpan);
+
+    const timer = setInterval(() => {
+      pLoad++;
       newSpan.innerText = "Loading game " + pLoad + "%";
 
-      gameBootstrapper.replaceWith(newSpan);
-    }
+      if (pLoad >= 100) {
+        clearInterval(timer);
+      }
+    }, 30);
   });
 }

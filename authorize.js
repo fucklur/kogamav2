5360e67a0189832551923abb818a5d98fdf79c1f
Fucklur <boryscuber13@gmail.com>
2026-08-12 13:33:21 +0200

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -2,6 +2,7 @@ const loginbutton = document.getElementById("loginbtn");
 
 if (loginbutton) {
   loginbutton.addEventListener("click", () => {
+  alert("Wrong username or password. Go to DevTools to see why")
   console.error("error unique id: " + Math.random() + " please refer to the administration here");
 });
 }
